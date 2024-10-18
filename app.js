4d46feea79d102a9aebfa4732e812f81a9b66fd0
natan-addis <nataddis21@gmail.com>
2024-10-18 08:46:06 -0400

    merged

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// backend/app.js
 const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
@@ -12,32 +11,17 @@ const corsOptions = {
   origin: '*', // Allow all origins
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specific methods
   credentials: true, // Allow credentials (e.g., cookies)
+  preflightContinue: false,
+  optionsSuccessStatus: 204 // Some legacy browsers choke on 204
 };
 
 app.use(cors(corsOptions)); // Use CORS middleware with options
 
-connectDB()
-  .then(() => {
-    console.log("Connected to MongoDB successfully");
-
-    // Start the server only after successful connection
-    app.listen(PORT, () => {
-      console.log(`Server started at http://localhost:${PORT}`);
-    });
-
-    // Import your routes here after connection
-    app.use("/", require("./routes/route")); 
-  })
-  .catch((error) => {
-    console.error("MongoDB connection error:", error);
-    process.exit(1);
-  });
-
-app.use(express.static("public"));
-app.use(express.static("uploads"));
+// Middleware for parsing request bodies
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Session configuration
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -46,6 +30,29 @@ app.use(
   })
 );
 
+// Middleware for serving static files
+app.use(express.static("public"));
+app.use(express.static("uploads"));
+
+// Connect to MongoDB and start the server
+connectDB()
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+
+    // Import your routes here after connection
+    app.use("/", require("./routes/route")); 
+
+    // Start the server only after successful connection
+    app.listen(PORT, () => {
+      console.log(`Server started at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
+
+// Middleware to handle session messages
 app.use((req, res, next) => {
   res.locals.message = req.session.message;
   delete req.session.message;
